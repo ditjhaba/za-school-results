5637fb5a23edd526e8d7efa0bffb986cda538f7f
Hendrik Louw <hendrik@Hendriks-MacBook-Pro.local>
2013-11-22 16:47:30 +0200

    Add pan control

--- frontend/app/scripts/views/map_view.js
+++ frontend/app/scripts/views/map_view.js
@@ -5,13 +5,17 @@ Frontend.MapView = Ember.View.extend({
   didInsertElement: function() {
 
    Frontend.map = window.L.map('map', {zoomControl: false}).setView([-30.559482,  22.937505999999985], 6 );
-   new window.L.Control.Zoom({ position: 'topright' }).addTo(Frontend.map);
 
+    window.L.control.zoom({position: 'topright'}).addTo(Frontend.map);
+   // window.L.control.pan().addTo(Frontend.map);
+   // // new window.L.Control.Zoom({ position: 'topright' }).addTo(Frontend.map);
     window.L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
         key: 'BC9A493B41014CAABB98F0471D759707',
         styleId: 22677
       }).addTo(Frontend.map);
+
+    window.L.control.pan({position: 'topright'}).addTo(Frontend.map);
 
     this.get('controller').send('drawOverlay');
   }
