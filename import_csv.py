ca3ea92eccfa38c8320fcd49553df4d8c7bc7565
Ditjhaba <edselemela@gmail.com>
2014-05-28 16:44:51 +0200

    [#] Dj && Greg && Charles: Renamed the method, improved it's implementation for extracting data from school master data list.

--- import_csv.py
+++ import_csv.py
@@ -4,15 +4,20 @@ import xlrd
 import csv
 import sys
 
-def slice_data_from_excel():
+def slice_data_from_csv():
 	if len(sys.argv) == 3:
 		with open(sys.argv[1], 'rb') as input_file, open(sys.argv[2], 'rb') as headers_file, open('output_file.csv', 'wb') as output_file:
 				input_data = csv.DictReader(input_file)
 				headers = [line for line in csv.reader(headers_file)]
 				output_file = csv.DictWriter(output_file, headers[0], extrasaction='ignore')
+				k=0
 				for line in input_data:
-					output_file.writerow(line)
+					if k>=1:
+						output_file.writerow(line)
+					k += 1
 	else:
+		print "********************************************************"
 		print "usage: python import_csv.py <input_file> <headers_file>"
+		print "********************************************************"
 
-slice_data_from_excel()
+slice_data_from_csv()
