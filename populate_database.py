a1c94add9e8e2aae707b10f15fafc9c16037ef1d
Charles Kimpolo <charles@aims.ac.za>
2014-07-08 16:35:13 +0200

    DJ & Charles: finished the python script to populate database and collections.

--- populate_database.py
+++ populate_database.py
@@ -1,11 +1,11 @@
-import sys
 import csv
 import json
+import sys
 
 from pymongo import Connection
 
 # *************************************************************************
-# Establishing the connection and creating a database and collections 
+# Establishing mongo connection and creating a database and collections 
 # *************************************************************************
 connection = Connection()
 db = connection['za_schools_db']
@@ -16,7 +16,7 @@ schools = db['schools']
 # *************************************************************************
 
 # *************************************************************************
-# Input Data and Headers Files
+# Paths for Data and Header Files
 # *************************************************************************
 DATA_PATH = "parse_csv/resources/raw_data/"
 matric_results_file = "{0}{1}".format(DATA_PATH, "sa_matric_school_results.csv")
@@ -28,14 +28,16 @@ sanitation_headers = "{0}{1}".format(DATA_PATH, "data_preparation/sa_schools_san
 schools_file = "{0}{1}".format(DATA_PATH, "sa_schools_master_list.csv")
 school_headers = "{0}{1}".format(DATA_PATH, "data_preparation/sa_schools_master_headers.csv")
 # *************************************************************************
+provinces_file = "{0}{1}".format(DATA_PATH, "sa_provinces.csv")
+province_headers = "{0}{1}".format(DATA_PATH, "data_preparation/sa_provinces_headers.csv")
+# *************************************************************************
 
 class Province(object):
 	"""Defining an object that represents a province"""
-	def __init__(self, _id, code=None, name=None, no_of_boys=None, 
+	def __init__(self, code, name=None, no_of_boys=None, 
 		        no_of_girls=None, passed=None, pass_rate=None, running_water=None, 
 		        sanitation_plan=None, total_toilets=None, wrote=None):
 		self.code = code
-		self._id = _id
 		self.name = name
 		self.no_of_boys = no_of_boys
 		self.no_of_girls = no_of_girls
@@ -67,7 +69,6 @@ class Sanitation(object):
 		self.sanitation_plan = sanitation_plan
 		self.total_toilets = total_toilets
 	
-
 class School(object):
 	"""Defining an object that represents a school"""
 	def __init__(self, emis, gis_lat=None, gis_lng=None, province_code=None, 
@@ -102,6 +103,9 @@ def read_csv_files(headers_file, data_file):
 
 			elif headers_file.__contains__("schools_master"):
 				populate_schools(line, headers)
+
+			elif headers_file.__contains__("provinces"):
+				populate_provinces(line, headers)
 				
 
 def populate_sanitations(data, header):
@@ -139,18 +143,84 @@ def populate_schools(data, header):
 					urban_rural=data[header.index("urban_rural")],
 					no_fee_school=data[header.index("no_fee_school")])
 	schools.insert(school.__dict__)
+
+def populate_provinces(data, header):
+	code = data[header.index("province")]
+	prov_schools_matric_result = schools.find({"province_name": code, 
+												"matric_result_emis": {"$ne": ""}})
+	prov_schools_sanitation = schools.find({"province_name": code, 
+												"sanitation_emis": {"$ne": ""}})
+
+	prov_sanitations = province_sanitations(prov_schools_sanitation)
+	prov_matric_results = province_matric_results(prov_schools_matric_result)
 	
-# *************************************************************************
-# Running the scripts to populate the 'za_schools' database and collections
-# *************************************************************************
+	province = Province(code=code, 
+						name=data[header.index("province_long")],
+						no_of_boys=prov_sanitations.get('no_of_boys'),
+						no_of_girls=prov_sanitations.get('no_of_girls'),
+						passed=prov_matric_results.get('passed'),
+						pass_rate=prov_matric_results.get('pass_rate'),
+						running_water=prov_sanitations.get('running_water'),
+						total_toilets=prov_sanitations.get('total_toilets'),
+						wrote=prov_matric_results.get('wrote'))
+	provinces.insert(province.__dict__)
+
+# *********************************************************************************
+# Extracting matric results for a province given its list of schools
+def province_matric_results(schools):
+	wrote = 0
+	passed = 0
+	for school in schools:
+		matric_result = matric_results.find_one({"emis": school.get('emis')})
+		wrote += int(matric_result.get('wrote'))
+		passed += int(matric_result.get('passed'))
+
+	return {"wrote": wrote, "passed": passed, 
+			"pass_rate": round((passed/float(wrote)) * 100, 2) if wrote!=0 else 0}
+# *********************************************************************************
+
+# *********************************************************************************
+# Extracting sanitation data for a province given its list of schools
+def province_sanitations(schools):
+	no_of_boys = 0
+	no_of_girls = 0
+	total_toilets = 0
+	running_water = 0
+
+	for school in schools:
+		sanitation = sanitations.find_one({"emis": school.get('emis')})
+		if not bad_data(sanitation.get('no_of_boys')):
+			no_of_boys += int(sanitation.get('no_of_boys'))
+		if not bad_data(sanitation.get('no_of_girls')):
+			no_of_girls += int(sanitation.get('no_of_girls'))
+		if not bad_data(sanitation.get('total_toilets')):
+			total_toilets += int(sanitation.get('total_toilets'))
+		if sanitation.get('running_water') in ["Yes", "Yes "]: # In our data there are schools with running water "Yes" and "Yes "
+			running_water = running_water + 1
+
+	return {"no_of_boys": no_of_boys, "no_of_girls": no_of_girls, "total_toilets": total_toilets, 
+			"running_water": (float(running_water)/schools.count()) * 100 if schools.count()!=0 else running_water}
+
+# *********************************************************************************
+# Filter for bad data in sanitation table
+def bad_data(value):
+	return (value in ["Pit toilets", "VIP toilets", ""])
+# *********************************************************************************
+	
+# *********************************************************************************
+# Running the scripts to populate the 'za_schools' database and all its collections
+# *********************************************************************************
 read_csv_files(matric_result_headers, matric_results_file)
 print "*************************************************************"
-print "Successfully Loaded Matric Results Data"
+print "Loading Matric Results Data - Done"
 print "*************************************************************"
 read_csv_files(sanitation_headers, sanitations_file)
-print "Successfully Loaded Sanitations Data"
+print "Loading Sanitations Data - Done"
 print "*************************************************************"
 read_csv_files(school_headers, schools_file)
-print "Successfully Loaded Schools Data"
+print "Loading Schools Data - Done"
 print "*************************************************************"
-# *************************************************************************
+read_csv_files(province_headers, provinces_file)
+print "Loading Provinces Data - Done"
+print "*************************************************************"
+# ********************************************************************************
