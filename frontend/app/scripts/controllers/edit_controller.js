740c9cb91afed285fc5526cdee3f8db55e7fff05
Ditjhaba <edselemela@gmail.com>
2014-07-09 11:11:46 +0200

    Dj && Charles: Adding the retrieval and displaying of the correct school for editing purposes.

--- frontend/app/scripts/controllers/edit_controller.js
+++ frontend/app/scripts/controllers/edit_controller.js
@@ -2,10 +2,8 @@ Frontend.EditController = Ember.ObjectController.extend({
         school:'',
         myBoolean: false,
         actions:{
-            retrieve_school: function(){
+            retrieve_school: function() {
                 this.set('school', this.content);
-                this.set('myBoolean', true);
-                console.log(this.get('myBoolean'));
             },
 
             save: function(){
